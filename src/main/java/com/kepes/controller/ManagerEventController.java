91d46e23ecc0f06af1ff862189c5d852fe1897b1
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-05-14 21:37:07 +0100

    feat: add greating endpoint

--- src/main/java/com/kepes/controller/ManagerEventController.java
+++ src/main/java/com/kepes/controller/ManagerEventController.java
@@ -1,4 +1,16 @@
 package com.kepes.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class ManagerEventController {
+
+    @GetMapping("/greeting")
+    public ResponseEntity<String> greet() {
+        return ResponseEntity.ok("Hello, from Zoltan's Event Manager!");
+    }
 }
