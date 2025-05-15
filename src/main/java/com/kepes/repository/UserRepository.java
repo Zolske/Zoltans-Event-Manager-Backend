4c9aa7b81d26f36e6a09c8aa9a567af98d909fb0
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-05-15 14:59:08 +0100

    feat: add 'repository' package with 'UserRepository' class

---
+++ src/main/java/com/kepes/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.kepes.repository;
+
+import com.kepes.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, String> {
+}
