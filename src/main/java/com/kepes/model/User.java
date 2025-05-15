6ffc5995dc9bea1760ef79e08bb2ce07547b1293
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-05-15 16:22:12 +0100

    refactor: rename table to 'app_user', add getters and setters

--- src/main/java/com/kepes/model/User.java
+++ src/main/java/com/kepes/model/User.java
@@ -4,10 +4,11 @@ import jakarta.persistence.*;
 import org.springframework.stereotype.Component;
 
 @Entity
-@Table(name="user")
+@Table(name="app_user")
 public class User {
 
     @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id_user")
     private String id_user;
 
@@ -20,4 +21,35 @@ public class User {
     @Column(name="picture_url")
     private String picture_url;
 
+    public String getId_user() {
+        return id_user;
+    }
+
+    public void setId_user(String id_user) {
+        this.id_user = id_user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPicture_url() {
+        return picture_url;
+    }
+
+    public void setPicture_url(String picture_url) {
+        this.picture_url = picture_url;
+    }
 }
