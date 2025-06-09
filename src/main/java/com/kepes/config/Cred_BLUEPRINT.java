36815e82429f51c6ebaf86b29e5fa5c942129b0f
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-06-09 15:01:47 +0100

    feat: add blueprint files for config

---
+++ src/main/java/com/kepes/config/Cred_BLUEPRINT.java
@@ -0,0 +1,13 @@
+package com.kepes.config;
+
+/**
+ * Rename this file/Class to 'Cred' and past in the 'Web client id'
+ */
+
+public class Cred_BLUEPRINT {
+    private static final String WEB_CLIENT_ID = "";
+
+    public String getWebClientId() {
+        return WEB_CLIENT_ID;
+    }
+}
